1d58314352bf475a72cb0286206cd0cbc430aa3c
Anu Tomy <anuatomy@gmail.com>
2018-06-21 11:33:20 -0400

    Initial Commit

--- src/com/web/jdbc/TestServlet.java
+++ src/com/web/jdbc/TestServlet.java
@@ -1,6 +1,10 @@
 package com.web.jdbc;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -14,19 +18,43 @@ import javax.servlet.http.HttpServletResponse;
 public class TestServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-    /**
-     * Default constructor. 
-     */
-    public TestServlet() {
-        // TODO Auto-generated constructor stub
-    }
+	//Define datasource/connection pool for Resource Injection
+			@Resource(name="jdbc/web_student_tracker")
+			private DataSource  dataSource;	
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		//Step 1: Set up the printWriter
+		PrintWriter out = response.getWriter();
+		response.setContentType("text/plain");
+		
+		//Step 2: Get a connection to the database
+		Connection myConn = null;
+		Statement myStmt = null;
+		ResultSet myRs = null;
+	
+		try {
+			myConn = dataSource.getConnection();
+			
+			//Step 3: Create SQL statements
+			String sql = "select * from student";
+			myStmt =  myConn.createStatement();
+			
+			//Step 4: Execute SQL query
+			myRs = myStmt.executeQuery(sql);
+			
+			//Step 5: Process the result set
+			while (myRs.next()) {
+				String email = myRs.getString("email");
+				out.println(email);
+			}
+		}
+		catch (Exception exc) {
+			exc.printStackTrace();
+		}
+		
 	}
 
 }
