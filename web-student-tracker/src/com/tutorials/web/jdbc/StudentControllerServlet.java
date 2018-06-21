3b07800d9d6cad267f663993bb5cc44aed05b913
Anu Tomy <anuatomy@gmail.com>
2018-06-21 18:49:35 -0400

    Build the Servlet

--- web-student-tracker/src/com/tutorials/web/jdbc/StudentControllerServlet.java
+++ web-student-tracker/src/com/tutorials/web/jdbc/StudentControllerServlet.java
@@ -1,11 +1,15 @@
 package com.tutorials.web.jdbc;
 
 import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.sql.DataSource;
 
 /**
  * Servlet implementation class StudentControllerServlet
@@ -13,13 +17,59 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/StudentControllerServlet")
 public class StudentControllerServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+	
+	//reference to the StudentDbUtil
+	private StudentDbUtil studentDbUtil;
+	
+	//Define datasource/connection pool for Resource Injection
+	@Resource(name = "jdbc/web_student_tracker")
+	private DataSource dataSource;
+	
+	//initialize StudentDbUtil
+	
+	@Override
+	public void init() throws ServletException {
+		// TODO Auto-generated method stub
+		super.init();
+		
+		//Create studentDbUtil and pass the conn pool/datasource
+		try {
+			studentDbUtil = new StudentDbUtil(dataSource);
+		}
+		catch(Exception exc) {
+			throw new ServletException(exc);
+		}
+		
+	}
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		
+		try {
+			// list the students...in mvc fashion
+			listStudents(request, response);
+		}
+		catch (Exception exc) {
+			throw new ServletException(exc);
+		}
 	}
+
+	private void listStudents(HttpServletRequest request, HttpServletResponse response) {
+		
+		//get students from db util
+		List<Student> students = studentDbUtil.getStudents();
+		
+		//add students to the request		
+		request.setAttribute("STUDENT_LIST", students);
+		
+		// send to JSP page(view)
+		
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/list-students.jsp");
+		dispatcher.forward(request, response);
+	}
+
+	
 
 }
