c33a397fcf388d471dc4c4c8be518a2cdf810ec3
Anu Tomy <anuatomy@gmail.com>
2018-06-25 19:44:46 -0400

    Add method deleteStudent()

--- web-student-tracker/src/com/tutorials/web/jdbc/StudentControllerServlet.java
+++ web-student-tracker/src/com/tutorials/web/jdbc/StudentControllerServlet.java
@@ -79,6 +79,9 @@ public class StudentControllerServlet extends HttpServlet {
 				updateStudent(request,response);
 				break;	
 				
+			case "DELETE":
+				deleteStudent(request,response);
+				break;				
 				
 			default:
 				listStudents(request, response);				
@@ -88,6 +91,18 @@ public class StudentControllerServlet extends HttpServlet {
 		catch (Exception exc) {
 			throw new ServletException(exc);
 		}
+	}
+
+	private void deleteStudent(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		
+		//read student id from form data
+		String theStudentId = request.getParameter("studentId");
+		
+		//delete student from database
+		studentDbUtil.deleteStudent(theStudentId);
+		
+		//send them back to "list students" page
+		listStudents(request,response);
 	}
 
 	private void updateStudent(HttpServletRequest request, HttpServletResponse response) throws Exception{
